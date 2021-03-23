b9c2a45ccfa73c9f0d5cda7d53267b4570cd666d
Yu Jin <susanna1221@naver.com>
2021-03-23 13:25:56 +0900

    히스토그램 한 화면에

--- drawLine.py
+++ drawLine.py
@@ -142,17 +142,17 @@ def getHisto(cornerinfo):
 
 
 # 히스토그램 이미지 출력
-def drawHisto(histo, title):
+def drawHisto(histo, k):
     rangeValue = []
     for i in range(int(360 / angleRange)):
         rangeValue.append(36 * i)
     x = np.arange(int(360 / angleRange))
-    plt.figure(figsize=(4, 4))
+    plt.subplot(241 + k)
     plt.bar(x, histo, width=1, align='center')
     plt.xticks(x, rangeValue)
     plt.xlim([0, 360 / angleRange])
     plt.ylim([0, 1])
-    plt.title(title)
+    plt.title(str(int(k / 4 + 1)) + '-' + str(k % 4 + 1))
 
 
 def cornerDetect():  # main function for corner detecting
@@ -211,9 +211,10 @@ def cornerDetect():  # main function for corner detecting
     # calculate Magnitude and Orientation
     result = get_Mag_Ori(corner, 5)
     histo = []
+    plt.figure(figsize=(16, 8))
     for k in range(len(result)):
         histo.append(getHisto(result[k]))
-        drawHisto(histo[k], str(int(k / 4 + 1)) + '-' + str(k % 4 + 1))
+        drawHisto(histo[k], k)
     plt.show()
 
     # 이미지 가로로 붙이기
